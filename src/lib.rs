b2b850cb01dd5cf563458ee15151444b8c69bf99
Hajime Fukuda <hajime.fukuda@me.com>
2017-02-23 00:44:03 +0900

    added pub use

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,14 @@ extern crate emval_sys;
 pub mod js_serializable;
 pub mod jsobj;
 
+pub use jsobj::{Args, JSObj};
+pub use js_serializable::JSSerializable;
+
 #[macro_export]
 macro_rules! args {
     ( $( $x:expr ),* ) => {{
+        use emval;
+
         let mut types = Vec::new();
         let mut values = Vec::new();
         $(
